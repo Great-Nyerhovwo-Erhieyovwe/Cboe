b3513ad4b2b793df9c823e52a41ca4eedf2e5827
Great Nyerhovwo Erhievovwe <erhieyovwegreat7@gmail.com>
2025-09-23 16:07:27 +0100

    serve static frontend files

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
+const express = require('express');
+const path = require('path');
 const jsonServer = require('json-server');
-const server = jsonServer.create();
+
+const app = express();
+
+// server static frontend file from 'public' folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Json server api
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
+app.use('/api', middlewares);
+app.use('/api', router);
+
+// Catch-all route (optional, for SPA)
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5500;
-
-server.use(middlewares);
-server.use(router);
-
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`JSON server is running on port ${PORT}`);
-})
+});
